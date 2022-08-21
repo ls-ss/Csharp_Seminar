a7084ba0d46569a4d58116d982af486dbc479c72
SergeyL <mkru@mail.ru>
2022-08-21 11:42:22 +0300

    Add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите целое положительное число больше 1: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("Список четных чисел: ");
+int count = 2;
+while (count <= n){
+    if (count % 2 == 0){
+        Console.Write($"{count}, ");
+    }
+    count++;
+}
+Console.WriteLine("");
